4f612b3a0cf6e0f7b4de241a6533265d20a79570
Luis Vega <pinluisvega@gmail.com>
2026-09-02 20:42:05 -0500

    update romanos

--- main.py
+++ main.py
@@ -6,9 +6,28 @@ from pathlib import Path
 app = FastAPI(title="Servicio de cédula")
 
 
+def int_to_roman(number: int) -> str:
+    values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    numerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+    roman = ""
+    for value, numeral in zip(values, numerals):
+        while number >= value:
+            roman += numeral
+            number -= value
+    return roman
+
+
 @app.get("/obtenercedula")
 def obtener_cedula() -> int:
     return random.randint(1_000_000_000, 9_999_999_999)
+
+
+@app.get("/numeroromano")
+@app.get("/romano")
+def obtener_numero_romano() -> str:
+    numero = random.randint(50, 100)
+    return int_to_roman(numero)
 
 
 @app.get("/swagger.html", response_class=HTMLResponse)
